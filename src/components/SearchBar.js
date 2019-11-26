72f356d9ba528a364f7e36d67bfb21f6ecadbe67
= <atulsingh1396@gmail.com>
2019-11-27 01:08:14 +0530

    Added axios18.0.1 and checked response from api

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -9,13 +9,13 @@ class SearchBar extends React.Component {
     this.setState({
       searchTerm: event.target.value
     });
-    console.log(event.target.value);
   };
   onFormSubmit = event => {
     // preventDefault() prevents auto page reload on form submission.
     event.preventDefault();
     // TODO: Remember to call the callback from
     // the parent to ensure successful submission.
+    this.props.onFormSubmit(this.state.searchTerm);
   };
   render() {
     return (
@@ -24,9 +24,9 @@ class SearchBar extends React.Component {
           <div className="field">
             <label>Search YouTubez</label>
             <input
+              onChange={this.onInputChange}
               type="text"
               value={this.state.searchTerm}
-              onChange={this.onInputChange}
             />
           </div>
         </form>
